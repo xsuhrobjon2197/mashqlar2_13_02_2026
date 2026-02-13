1329c7808519b07ea25c4bf76319dc178e7dd2d0
xsuhrobjon2197 <xsuhrobjon2197@gmail.com>
2026-02-13 16:00:43 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+# #1.6-masala
+# values = (12,5, 8,19,3, 15)
+# values1 = max(values)
+# values2 = min(values)
+# print(values1)
+# print(values2)
+# values_new = values1 - values2
+# print(values_new)
+
+# #1.7-masala
+# words = ("apple", "banana", "cherry", "date")
+# new_words = words[::-1]
+# print(new_words)
+# print(new_words)
+# print(new_words[0])
+# print(new_words[-1])
+
+# #1.8-masala
+# data = (10, 20, 30, 40, 50)
+# if 30 in data:
+#     index = data.index(30)
+#     print(f"30 soni mavjud, indeksi: {index}")
+# else:
+#     print("Element topilmadi")
+
+# #1.9-masala
+# nums = (1, 2, 3, 4, 5, 6, 7, 8)
+# juft_sonlar = tuple(x for x in nums if x % 2 == 0)
+# print(juft_sonlar)
+# juft_soni = len(juft_sonlar)
+# print(juft_soni)
+
+# #1.10-masala
+# nested_tuple = ((1, 2), (3, 4), (5, 6))
+# umumiy_yigindi = sum(a + b for a, b in nested_tuple)
+# print("Barcha sonlar yig'indisi:", umumiy_yigindi)
+# ichki_yigindilar = tuple(a + b for a, b in nested_tuple)
+# print("Har bir ichki tuple yig'indilari:", ichki_yigindilar)
+
+# #1.11-masala
+# fruits = ("apple", "kiwi", "banana", "pear")
+# fruits_new = list(fruits)
+# fruits_new_2 = len(fruits_new)
+# print(fruits_new)
+# print(fruits_new_2)
